a22721e4c114d2fab2c890280dd6db21385d2cd9
letanloc1941995 <letanloc1941995@gmail.com>
2021-02-18 09:22:16 +0700

    create single event

--- app/src/main/java/locle/android/com/mvvm_cleanarchitechture/utils/Event.kt
+++ app/src/main/java/locle/android/com/mvvm_cleanarchitechture/utils/Event.kt
@@ -1,5 +1,9 @@
 package locle.android.com.mvvm_cleanarchitechture.utils
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
 open class Event<out T>(private val content: T) {
 
     var hasBeenHandled = false
@@ -21,4 +25,12 @@ open class Event<out T>(private val content: T) {
      * Returns the content, even if it's already been handled.
      */
     fun peekContent(): T = content
+}
+
+// Live Data Ext
+fun <T> LiveData<out Event<T>>.observeSingleEvent(
+    owner: LifecycleOwner,
+    onEventUnhandled: (T) -> Unit
+) {
+    observe(owner, Observer { it?.getContentIfNotHandled()?.let(onEventUnhandled) })
 }
